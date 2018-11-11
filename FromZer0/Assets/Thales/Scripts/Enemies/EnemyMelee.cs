144ae26abf71c87e5077597098d4ddd4d5722359
Pedro Alexandre da Costa Alcoforado <pedrotkdo@gmail.com>
2018-11-11 22:38:12 +0000

    MeeleeMan
    implementation of the coolest melee dude

---
+++ FromZer0/Assets/Thales/Scripts/Enemies/EnemyMelee.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_EnemyMelee : MonoBehaviour {
+
+    public float speed;
+    public int AnimAtackSpeed;
+    Animator myAnimator;
+    Collider2D swordcol;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.speed = AnimAtackSpeed;
+
+        swordcol = GameObject.Find("EnemySword").GetComponent<Collider2D>(); 
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //Take Damage to the player 
+        }
+    }
+
+    private void AttackAnimation()
+    {
+ 
+        myAnimator.SetTrigger("attack");
+    }
+
+    public void Attack()
+    {
+
+        InvokeRepeating("AttackAnimation", 2.0f, 0.3f);
+    }
+
+
+    void StartAttackAnim()
+    {
+        swordcol.enabled = true;
+    }
+
+    void EndAttackAnim()
+    {
+        swordcol.enabled = false;
+        myAnimator.ResetTrigger("attack");
+    }
+}
--- FromZer0/Assets/Thales/Scripts/Enemies/MeleeEnemyMov.cs
+++ FromZer0/Assets/Thales/Scripts/Enemies/MeleeEnemyMov.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeEnemyMov : MonoBehaviour {
+public class P_MeleeEnemyMov : MonoBehaviour {
 
     public float speed;
     EnemyMelee EM;
@@ -32,9 +32,9 @@ public class MeleeEnemyMov : MonoBehaviour {
             CheckPlayerPosition();
             
 
-            if (Vector2.Distance(transform.position, collision.gameObject.transform.position) > 2)
+            if (Vector2.Distance(transform.position, Player.transform.position) > 2)
             {
-                transform.position = Vector2.MoveTowards(transform.position, collision.gameObject.transform.position, speed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
             }
         }
     }
@@ -49,13 +49,13 @@ public class MeleeEnemyMov : MonoBehaviour {
             print("left");
 
             Enemy.transform.localRotation = Quaternion.Euler(0, 0, 0);
-            EnemySwordChild.GetComponent<EnemyMelee>().Attack();
+            EnemySwordChild.GetComponent<P_EnemyMelee>().Attack();
         }
         else if (relativePoint.x > 0)
         {
             print("right");
             Enemy.transform.localRotation = Quaternion.Euler(0, 180, 0);
-            EnemySwordChild.GetComponent<EnemyMelee>().Attack();
+            EnemySwordChild.GetComponent<P_EnemyMelee>().Attack();
 
         }
         else
