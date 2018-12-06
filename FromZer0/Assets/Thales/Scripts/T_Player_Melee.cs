379395a85b42c2cd104085f68f4a7480f773111c
Thales Martins Neto <thalefeather@gmail.com>
2018-12-06 17:32:07 +0000

    combo system

--- FromZer0/Assets/Thales/Scripts/T_Player_Melee.cs
+++ FromZer0/Assets/Thales/Scripts/T_Player_Melee.cs
@@ -9,9 +9,17 @@ public class T_Player_Melee : MonoBehaviour {
     [SerializeField] float attackRate = 0.5f;
     [SerializeField] float attackRange = 2f;
     [Space]
-    [SerializeField] float currentComboAttack = 0;
-    [SerializeField] float amountOfTimeToComboIntoNextAttack = 1;
-    [SerializeField] float breakComboStringCounter = 1;
+    //[SerializeField] float currentComboAttack = 0;
+    //[SerializeField] float amountOfTimeToComboIntoNextAttack = 1;
+    //[SerializeField] float breakComboStringCounter = 1;
+    [Space]
+    public int noOfClicks = 0;//Time when last button was clicked
+    public float lastClickedTime = 0;//Delay between clicks for which clicks will be considered as combo
+    public float maxComboDelay = 0.2f;
+    public float spaceBetweenClicks = 0;
+    [Space]
+    public float delayCombo1to2 = 0.1f;
+    public float delayCombo2to3 = 0.1f;
     [Space]
     [SerializeField] GameObject slash0;
     [SerializeField] GameObject slash1;
@@ -22,14 +30,22 @@ public class T_Player_Melee : MonoBehaviour {
         //attack();
         attackCounter = attackCounter - Time.deltaTime;
 
-        breakComboStringCounter = breakComboStringCounter - Time.deltaTime;
+        //breakComboStringCounter = breakComboStringCounter - Time.deltaTime;
 
+
+
+        /*
         if(breakComboStringCounter < 0)
         {
             currentComboAttack = 0;
+        }*/
+
+        if (Time.time - lastClickedTime > maxComboDelay)
+        {
+            noOfClicks = 0;
         }
 
-        if(currentComboAttack == 0)
+        if (noOfClicks == 0)
         {
             this.GetComponent<T_Player_Inputs>().inCombo = false;
         }
@@ -37,9 +53,63 @@ public class T_Player_Melee : MonoBehaviour {
         {
             this.GetComponent<T_Player_Inputs>().inCombo = true;
         }
+        
     }
 
     public void attack()
+    {
+        //Debug.Log("ATTACK IS CALLED");
+        //Record time of last button click
+        spaceBetweenClicks = Time.time - lastClickedTime;
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if (noOfClicks == 1 && attackCounter <= 0)
+        {
+            //animator.SetBool("Attack1", true);
+            executeAttack1();
+        }
+        if (noOfClicks == 1 && attackCounter > 0)
+        {
+            noOfClicks = 0;
+        }
+        if (noOfClicks == 2)
+        {
+            //animator.SetBool("Attack1", true);
+            executeAttack2();
+            //Invoke("executeAttack2", delayCombo1to2);
+        }
+        if (noOfClicks == 3)
+        {
+            //animator.SetBool("Attack1", true);
+            executeAttack3();
+            //Invoke("executeAttack3", delayCombo2to3);
+            noOfClicks = 0;
+            attackCounter = attackRate;
+        }
+        
+        //limit/clamp no of clicks between 0 and 3 because you have combo for 3 clicks
+        noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+    }
+
+    private void executeAttack1()
+    {
+        GameObject attackObject = Instantiate(slash0, this.transform.position + this.transform.up.normalized/attackRange, this.transform.rotation);
+    }
+
+    private void executeAttack2()
+    {
+        GameObject attackObject = Instantiate(slash1, this.transform.position + this.transform.up.normalized / attackRange, this.transform.rotation);
+    }
+
+    private void executeAttack3()
+    {
+        GameObject attackObject = Instantiate(slash2, this.transform.position + this.transform.up.normalized / attackRange, this.transform.rotation);
+    }
+
+
+    /////////
+
+    /*void OldAttack()
     {
         Debug.Log("ATTACK IS CALLED");
 
@@ -68,20 +138,5 @@ public class T_Player_Melee : MonoBehaviour {
             //attackCounter = attackRate;
             Debug.Log("slash 0");
         }
-    }
-
-    private void executeAttack0()
-    {
-        GameObject attackObject = Instantiate(slash0, this.transform.position + this.transform.up.normalized/attackRange, this.transform.rotation);
-    }
-
-    private void executeAttack1()
-    {
-        GameObject attackObject = Instantiate(slash1, this.transform.position + this.transform.up.normalized / attackRange, this.transform.rotation);
-    }
-
-    private void executeAttack2()
-    {
-        GameObject attackObject = Instantiate(slash2, this.transform.position + this.transform.up.normalized / attackRange, this.transform.rotation);
-    }
+    }*/
 }
